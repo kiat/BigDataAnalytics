b593d022cea897dab238e14c53a522d5a2f6e20f
Kia Teymourian <kiat@Kias-MacBook-Pro.local>
2018-09-19 02:08:35 -0400

    added an example about TPCH

---
+++ TPC-H-Example/TPCH-Example.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark import SparkContext
+from pyspark import SparkConf,SparkContext
+from pyspark.streaming import StreamingContext
+import sys
+import requests
+from operator import add
+
+from pyspark.sql.types import *
+from pyspark.sql import functions as func
+from pyspark.sql.functions import lit
+
+
+
+
+if __name__ == "__main__":
+    
+
+    # create spark configuration
+    conf = SparkConf(appName="TPCH-Example")
+
+    # create spark context with the above configuration
+    sc = SparkContext(conf=conf)
+
+
+    # customers = sqlContext.read.format('csv').options(header='true', inferSchema='true',  sep ="|").load("file:////Users/kiat/git/MET-CS777/data/tpch_tables_scale_0.1/customer.tbl")
+
+    orders = sqlContext.read.format('csv').options(header='true', inferSchema='true',  sep ="|").load(sys.arg[1])
+
+
+
+    
+    lines = lineitems.select("ORDERKEY", "PARTKEY")\
+    .withColumn("COUNT", lit(1))
+    .groupBy("PARTKEY").agg(func.sum("COUNT"))\
+
+    result_1 = lines.orderBy("sum(COUNT)", ascending=False).limit(10).show()
+
+
+
+    result_1.saveAsTextFile(sys.arg[2])
+
+
+
+# Question 2
+# Implement a pyspark code that can find out the top-10 sold products. 
+
+
+# Something like this with RDD... 
+# orders.map(lambda p: (p[0], p[2]*p[3]) ).reduceByKey(lambda (a,b): a+b).top(10, lambda x: x[1])
+
+
+
+
+
+# ---------------------------------------------------------------------------
+# Question 3
+
+# Find the top-10 customers based on the number of products ordered.
+
+
+
+
+
+# ---------------------------------------------------------------------------
+
+# Question 4
+# Find the top-10 customers that have ordered products from the same supplier. 
+
+
+
+
+
+# ---------------------------------------------------------------------------
+# Question 5 and 6 
+# Find the customers who have not ordered
+# products from their own country and have ordered only foreign products. 
+
+# Get customer and Orderd countries from product table 
+
+
+
+
+
+# ---------------------------------------------------------------------------
+# Question 7
+# Find the top-10 similar customers based of their orders. (Jaccard Similarity)
+# First of all we collect all of the products that each customer ordered. 
+
+
+
+
+# ---------------------------------------------------------------------------
+# Question 8
+# Implement a pyspark code that can find the top-10 products pairs that are ordered mostly together. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    sc.stop()
