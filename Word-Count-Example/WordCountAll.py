b08ed387128d9e57c8a9a2ccd901e59a90d907aa
Kia Teymourian <kiat@bu.edy>
2020-07-25 17:43:02 -0400

    upedate

---
+++ Word-Count-Example/WordCountAll.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import sys
+from operator import add
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: wordcount <file> <output> ", file=sys.stderr)
+        exit(-1)
+
+    sc = SparkContext(appName="PythonWordCount")
+    lines = sc.textFile(sys.argv[1])
+
+    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
+
+
+    counts.saveAsTextFile(sys.argv[2])
+
+    sc.stop()
